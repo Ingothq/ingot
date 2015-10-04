8b0750722fd171774a5d5ea510d39aeac124603f
Josh Pollock <jpollock412@gmail.com>
2015-10-04 15:45:48 -0400

    setup unit tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+$_SERVER['SERVER_NAME'] = '';
+$PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
+
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+
+require_once $_tests_dir . '/includes/functions.php';
+
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../ingot.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require $_tests_dir . '/includes/bootstrap.php';
+
+activate_plugin( 'ingot/ingot.php' );
+
+
+global $current_user;
+
+$current_user = new WP_User(1);
+$current_user->set_role('administrator');
+
+// Include helpers
---
+++ tests/tests-tests.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @TODO What this does.
+ *
+ * @package   @TODO
+ * @author    Josh Pollock <Josh@JoshPress.net>
+ * @license   GPL-2.0+
+ * @link
+ * @copyright 2015 Josh Pollock
+ */
+class tests extends \WP_UnitTestCase {
+
+	public function setUp() {
+
+	}
+
+	public function tearDown() {
+
+	}
+
+	public function testTests() {
+		$this->assertEquals( 1, 1 );
+
+	}
+
+}
