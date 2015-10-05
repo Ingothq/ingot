deef305b0a5e6e8224ad7a9a42c00a281007f973
Josh Pollock <jpollock412@gmail.com>
2015-10-04 20:50:48 -0400

    Move get_all from group crud to options_crud so we can re-use in test crud

--- classes/testing/crud/group.php
+++ classes/testing/crud/group.php
@@ -14,43 +14,6 @@ namespace ingot\testing\crud;
 
 class group extends options_crud {
 
-	/**
-	 * Get a collection of items
-	 *
-	 * @since 0.0.5
-	 *
-	 * @param array $params {
-	 *  $ids array Optional. Array of ids to get
-	 *  $limit int Optional. Limit results, default is -1 which gets all. Ignored if $ids is used.
-	 *  $page int Optional. Page of results, used with $limit. Default is 1. Ignored if $ids is used.
-	 * }
-	 *
-	 * @return array
-	 */
-	public static function get_items( $params ) {
-		if( ! empty( $params[ 'ids' ] ) ) {
-			return self::select_by_ids( $params[ 'ids' ] );
-		}
-		$limit = $page = 1;
-		$args = wp_parse_args(
-			$params,
-			array(
-				'ids' => array(),
-				'limit' => -1,
-				'page' => 1,
-			)
-		);
-
-		if ( -1 == $args[ 'limit' ] ) {
-			//@todo better hack?
-			$args[ 'limit' ] = 90000001;
-		}
-
-		extract( $args );
-		return self::get_all( $limit, $page );
-
-	}
-
 
 	/**
 	 * Name of this object
