f645b62597f69dbc328f645fc2fda5e97002efd0
Josh Pollock <jpollock412@gmail.com>
2015-10-11 08:21:43 -0400

    Finish sequence object

--- classes/testing/object/sequence.php
+++ classes/testing/object/sequence.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @TODO What this does.
+ * Object representing a sequence with extra, calculated data not in DB.
  *
- * @package   @TODO
+ * @package   ingot
  * @author    Josh Pollock <Josh@JoshPress.net>
  * @license   GPL-2.0+
  * @link
@@ -14,8 +14,154 @@ namespace ingot\testing\object;
 
 class sequence {
 
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
+	private $a_id;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
+	private $b_id;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var string
+	 */
+	private $test_type;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
 	private $a_win;
 
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
+	private $b_win;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
+	private $a_total;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
+	private $b_total;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
+	private $initial;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var bool
+	 */
+	private $completed;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
+	private $threshold;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var string
+	 */
+	private $created;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var string
+	 */
+	private $modified;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
+	private  $group_ID;
+
+	/**
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access private
+	 *
+	 * @var int
+	 */
+	private $total;
+
+
+	/**
+	 * Extra properties, not stored in DB, we create dynamically as needed.
+	 *
+	 * @since 0.0.7
+	 *
+	 * @var array
+	 */
 	private $extra_fields = array(
 		'a_win_percentage',
 		'b_win_percentage',
@@ -25,24 +171,64 @@ class sequence {
 		'win_total'
 	);
 
+	/**
+	 * Constructor for class
+	 *
+	 * @since 0.0.7
+	 *
+	 * @param array|int $sequence Sequence array as returned by CRUD class or sequence ID.
+	 */
 	public function __construct( $sequence ) {
 		$sequence = $this->verify_sequence( $sequence );
 		if( $sequence ){
 			$this->set_properties( $sequence );
+			$this->set_total();
 		}
 
 	}
 
+	/**
+	 * Magic getter.
+	 *
+	 * @since 0.0.7
+	 *
+	 * @param string $field Name of field
+	 *
+	 * @return mixed Field value
+	 */
 	public function __get( $field ) {
 		if( in_array( $field, $this->allowed_fields() ) ) {
 			if( isset( $this->$field ) ) {
 				return $this->$field;
 			}else{
-				$this->$field = call_user_func( array( $this, $field ) );
-				return $this->$field;
+				return call_user_func( array( $this, $field ) );
 			}
+
+		}elseif( 'sequence' == $field ) {
+			$sequence = array();
+			foreach( \ingot\testing\crud\sequence::get_all_fields() as $field ) {
+				$sequence[ $field ] = $this->$field;
+			}
+
+			return $sequence;
+
+		}else{
+			return false;
 		}
+
 	}
+
+	/**
+	 * Verify sequence, loading from DB if needed.
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access protected
+	 *
+	 * @param array|int $sequence Sequence array as returned by CRUD class or sequence ID.
+	 *
+	 * @return array|void
+	 */
 	protected function verify_sequence( $sequence ) {
 		if( is_numeric( $sequence ) ) {
 			$sequence = \ingot\testing\crud\sequence::read( $sequence );
@@ -50,10 +236,20 @@ class sequence {
 
 		if( ! is_wp_error( $sequence ) && ! empty( $sequence ) ) {
 			return $sequence;
+
 		}
 
 	}
 
+	/**
+	 * Calcualte and return an array of allowed fields for __get()
+	 *
+	 * @since 0.0.7
+	 *
+	 * @access protected
+	 *
+	 * @return array
+	 */
 	protected function allowed_fields() {
 		return array_merge( \ingot\testing\crud\sequence::get_all_fields(),$this->extra_fields );
 	}
@@ -61,7 +257,14 @@ class sequence {
 	private function set_properties( $sequence ) {
 
 		foreach( \ingot\testing\crud\sequence::get_all_fields() as $field ) {
+			//@todo why isn't this happening automattically?
+			if( ! in_array( $field, array( 'test_type', 'created', 'modified' ) ) ) {
+				$sequence[ $field ] = (int) $sequence[ $field ];
+			}
+
+
 			$this->$field = $sequence[ $field ];
+
 		}
 
 	}
@@ -73,10 +276,10 @@ class sequence {
 	 *
 	 * @access protected
 	 *
-	 * @return float
+	 * @return int
 	 */
 	protected function a_win_percentage() {
-
+		return $this->percentage( $this->a_win / $this->total );
 	}
 
 	/**
@@ -86,10 +289,10 @@ class sequence {
 	 *
 	 * @access protected
 	 *
-	 * @return float
+	 * @return int
 	 */
 	protected function b_win_percentage() {
-
+		return $this->percentage( $this->b_win / $this->total );
 	}
 
 	/**
@@ -99,10 +302,10 @@ class sequence {
 	 *
 	 * @access protected
 	 *
-	 * @return float
+	 * @return int
 	 */
 	protected function a_total_percentage() {
-
+		return $this->percentage( $this->a_total / $this->total );
 	}
 
 	/**
@@ -112,14 +315,18 @@ class sequence {
 	 *
 	 * @access protected
 	 *
-	 * @return float
+	 * @return int
 	 */
 	protected function b_total_percentage() {
+		return $this->percentage( $this->b_total / $this->total );
+	}
 
+	protected function percentage( $float ){
+		return 100 * $float;
 	}
 
 	/**
-	 * Calcualte total number of times tests of this sequence have ran.
+	 * Set total property
 	 *
 	 * @since 0.0.7
 	 *
@@ -127,26 +334,10 @@ class sequence {
 	 *
 	 * @return int
 	 */
-	protected function total() {
+	protected function set_total() {
+		$this->total = $this->a_total + $this->b_total;
 
 	}
-
-	/**
-	 * Calcualte total number of times tests of this sequence have won.
-	 *
-	 * @since 0.0.7
-	 *
-	 * @access protected
-	 *
-	 * @return int
-	 */
-	protected function total() {
-
-	}
-
-
-
-
 
 
 }
