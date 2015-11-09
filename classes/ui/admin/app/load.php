d548705f3ae31de6733335676754fbf332dbcdef
Josh Pollock <jpollock412@gmail.com>
2015-11-08 21:32:48 -0500

    Clean up load class

--- classes/ui/admin/app/load.php
+++ classes/ui/admin/app/load.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @TODO What this does.
+ * Load our admin Angular App
  *
- * @package   @TODO
+ * @package   ingot
  * @author    Josh Pollock <Josh@JoshPress.net>
  * @license   GPL-2.0+
  * @link
@@ -17,25 +17,40 @@ use ingot\testing\utility\helpers;
 
 class load {
 
+	/**
+	 * Menu slug
+	 *
+	 * @since 0.2.0
+	 *
+	 * @access protected
+	 *
+	 * @var string
+	 */
 	protected $menu_slug = 'ingot-admin-app';
 
-
+	/**
+	 * Constructors this class
+	 *
+	 * @since 0.2.0
+	 */
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_menu' ) );
 		if( $this->menu_slug === helpers::v( 'page', $_GET, 0 ) ) {
-			add_action( 'admin_enqueue_scripts', array( $this, 'scripts' ) );
-			add_action( 'admin_head', array( $this, 'inline' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'scripts' ) );;
 		}
 	}
 
 	/**
 	 * Add menu page
+	 *
+	 * @uses "admin_menu"
+	 *
+	 * @since 0.2.0
 	 */
 	public function add_menu() {
-
 		add_menu_page(
-			__( 'Ingot', 'ingot'),
-			__( 'Ingot', 'ingot'),
+			__( 'Ingot', 'ingot' ),
+			__( 'Ingot', 'ingot' ),
 			'manage_options',
 			$this->menu_slug,
 			array( $this, 'ingot_page' ),
@@ -44,6 +59,9 @@ class load {
 		);
 	}
 
+	/**
+	 *
+	 */
 	public function scripts() {
 
 
@@ -60,26 +78,25 @@ class load {
 
 		wp_enqueue_script( 'ingot-admin-app', INGOT_URL . 'assets/admin/js/admin-app.js', array( 'angular', 'jquery', 'swal' ), rand() );
 
-		wp_localize_script( 'ingot-admin-app', 'INGOT_ADMIN', array(
-				'api' => esc_url_raw( util::get_url() ),
-				'nonce' => wp_create_nonce( 'wp_rest' ),
-				'partials' => esc_url_raw( INGOT_URL . 'assets/admin/partials/' )
-			)
+		//data to use in admin app
+		wp_localize_script( 'ingot-admin-app', 'INGOT_ADMIN', $this->vars()
+		);
+
+		//translation strings for admin app
+		wp_localize_script( 'ingot-admin-app', 'INGOT_I10N', $this->translation_strings()
 		);
 
 
 
 	}
 
-	public function inline() {
-		return;
-		?>
-		<script type="text/javascript">
-			angular.element( document.getElementsByTagName( 'head' ) ).append( angular.element( '<base href="' + window.location.pathname + '" />' ) );
-		</script>
-		<?php
-	}
-
+	/**
+	 * Initial markup for admin page
+	 *
+	 * @TODO move this into a partial
+	 *
+	 * @since 0.2.0
+	 */
 	public function ingot_page() {?>
 		<div class="container" id="ingot-admin-app" ng-app="ingotApp">
 			<nav class="navbar navbar-default">
@@ -102,13 +119,9 @@ class load {
 							<li><a href="#"><?php _e( 'Settings', 'ingot' ); ?></a></li>
 							<li><a href="#"><?php _e( 'Support', 'ingot' ); ?></a></li>
 						</ul>
-					</div><!--/.nav-collapse -->
-				</div><!--/.container-fluid -->
+					</div>
+				</div>
 			</nav>
-			<div ng-controller="alerts">
-				<uib-alert ng-repeat="alert in alerts" type="{{alert.type}}" close="closeAlert($index)">{{alert.msg}}</uib-alert>
-
-			</div>
 			<div ui-view></div>
 
 		</div>
@@ -116,13 +129,60 @@ class load {
 	<?php
 	}
 
-	protected function admin_url( $hash = false) {
-		$url = add_query_arg( 'page', $this->menu_slug, admin_url( 'admin.php' ) );
-		if( $hash ) {
-			$url = untrailingslashit( $url ) . $hash;
-		}
 
-		return $url;
+
+	/**
+	 * Translation strings
+	 *
+	 * @since 0.2.0
+	 *
+	 * @access protected
+	 *
+	 * @return array
+	 */
+	protected function translation_strings() {
+		return array(
+			'api_url'                  => rest_url( 'ingot/v1' ),
+			'test_group_page_title'    => __( 'Ingot Test Group: ', 'ingot' ),
+			'success'                  => __( 'Group Saved', 'ingot' ),
+			'fail'                     => __( 'Could Not Save', 'ingot' ),
+			'close'                    => __( 'Close', 'ingot' ),
+			'saved'                    => __( 'Saved Group: ', 'ingot' ),
+			'cant_remove'              => __( 'At this time, you can not remove a test from a group.', 'ingot' ),
+			'beta_error_header'        => __( 'Beta Limitation Encountered', 'ingot' ),
+			'no_stats'                 => __( 'We do not have a functional stats viewer yet.', 'ingot' ),
+			'deleted'                  => __( 'Test Group Deleted', 'ingot' ),
+			'are_you_sure'             => __( 'Are You Sure About That?', 'ingot' ),
+			'delete_confirm'           => __( 'Deleting all groups is not reversible or undoable.', 'ingot' ),
+			'delete'                   => __( 'Delete', 'ingot' ),
+			'cancel'                   => __( 'Cancel', 'ignot' ),
+			'canceled'                 => __( 'Canceled', 'ingot' ),
+			'spinner_alt'              => __( 'Loading Spinner', 'ingot' ),
+			'no_tests'                 => __( 'This group has no tests', 'ingot' ),
+			'invalid_price_test_range' => __( 'Please enter a number between -.99 and .99', 'ingot' ),
+
+		);
+	}
+
+	/**
+	 * Data needed in app
+	 *
+	 * @since 0.2.0
+	 *
+	 * @access protected
+	 *
+	 * @return array
+	 */
+	protected function vars() {
+		return array(
+			'api'                 => esc_url_raw( util::get_url() ),
+			'nonce'               => wp_create_nonce( 'wp_rest' ),
+			'partials'            => esc_url_raw( INGOT_URL . 'assets/admin/partials/' ),
+			'spinner_url'         => trailingslashit( INGOT_URL ) . 'assets/img/loading.gif',
+			'edd_active'          => esc_attr( ingot_is_edd_active() ),
+			'woo_active'          => esc_attr( ingot_is_woo_active() ),
+			'price_tests_enabled' => esc_attr( ingot_enable_price_testing() )
+		);
 	}
 
 }
