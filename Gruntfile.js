fe963d9aaec290fe2361d11dd13cc8972fa2ce37
Josh Pollock <jpollock412@gmail.com>
2015-12-26 15:21:03 -0500

    prefer dist in composer when building #113

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function (grunt) {
         pkg     : grunt.file.readJSON( 'package.json' ),
         shell: {
             composer: {
-                command: 'composer update --no-dev'
+                command: 'composer update --no-dev --optimize-autoloader --prefer-dist'
             },
             bower: {
                 command: 'bower update'
