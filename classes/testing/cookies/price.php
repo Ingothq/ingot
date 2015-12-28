f70fef53072c3a0eb5fee67b37621eae6a18d144
Josh Pollock <jpollock412@gmail.com>
2015-12-28 01:32:53 -0500

    layout new price cookie class #126

--- classes/testing/cookies/price.php
+++ classes/testing/cookies/price.php
@@ -11,6 +11,8 @@
 
 namespace ingot\testing\cookies;
 
+use ingot\testing\crud\group;
+use ingot\testing\types;
 use ingot\testing\utility\helpers;
 
 class price {
@@ -26,6 +28,11 @@ class price {
 	 */
 	private $price_cookie;
 
+	/**
+	 * @var array
+	 */
+	private $tests;
+
 
 	/**
 	 * Construct object
@@ -37,11 +44,7 @@ class price {
 	 * @param bool $reset Optional. Whether to rest or not, default is false
 	 */
 	public function __construct( $current_sequences, $price_cookie, $reset = true ){
-		if ( false == $reset  ) {
-			$this->price_cookie = $price_cookie;
-		}else{
-			$this->price_cookie = [];
-		}
+		$this->set_price_cookie( $price_cookie, $reset );
 
 	}
 
@@ -55,7 +58,57 @@ class price {
 	public function get_price_cookie() {
 		return $this->price_cookie;
 	}
-	
+
+
+	protected function get_tests(){
+		$key = md5( __FUNCTION__ );
+		if( WP_DEBUG || ! is_array( $tests = get_transient( $key ) ) && ! empty( $tests ) ){
+			$tests = group::get_items( [ 'type' => 'price' ] );
+			if ( is_array( $tests ) ) {
+				$this->tests = $tests;
+				set_transient( $key, $tests, HOUR_IN_SECONDS );
+			}
+		}
+
+		return $tests;
+
+	}
+
+	protected function setup_price_cookie(){
+		foreach( $this->tests as $test ) {
+			if( $this->needed_to_add( $test ) ){
+				$this->add_test( $test );
+			}
+		}
+	}
+
+	protected function needed_to_add( $test ){
+		if( ! empty( $this->price_cookie )  ) {
+			if( ! isset( $this->price_cookie[ $test[ 'sub_type' ] ], $this->price_cookie[ $test[ 'sub_type' ] ][ $test[ 'ID' ] ] ) ){
+				return true;
+
+			}else{
+				$expires = $this->price_cookie[ $test[ 'sub_type' ] ][ $test[ 'ID' ] ][ 'expires' ];
+				if( $expires < $this->expires() ) {
+					return true;
+
+				}
+
+			}
+		}
+	}
+
+	protected function add_test( $test ){
+		$varinat = '';//need a price test bandit class
+
+		return [
+			'plugin' => $test[ 'sub_type' ],
+			'ID' => $test[ 'ID' ],
+			'variant' => $variant,
+			'expries' =>$this->expires(),
+
+		];
+	}
 	/**
 	 * Get expiration time for tests
 	 *
@@ -70,6 +123,24 @@ class price {
 	protected function expires() {
 		return time() + ( 10 * DAY_IN_SECONDS );
 
+	}
+
+	/**
+	 * @param $price_cookie
+	 * @param $reset
+	 */
+	private function set_price_cookie( $price_cookie, $reset ) {
+		if ( false == $reset ) {
+			$this->price_cookie = $price_cookie;
+		} else {
+			$this->price_cookie = [ ];
+		}
+
+		foreach ( types::allowed_price_types() as $type ) {
+			if ( ! isset( $this->price_cookie[ $type ] ) ) {
+				$this->price_cookie[ $type ] = [ ];
+			}
+		}
 	}
 
 }
