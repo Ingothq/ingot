8585a3a5d8427fc5b0027e657db3d36fc2c951ef
Josh Pollock <jpollock412@gmail.com>
2015-12-31 22:05:35 -0500

    Rearange loading so EDD SL stuff doesn't make errors when plugin can't load #130

--- ingot.php
+++ ingot.php
@@ -17,8 +17,18 @@ define( 'INGOT_DIR', dirname( __FILE__ ) );
 define( 'INGOT_UI_PARTIALS_DIR', dirname( __FILE__ ) . '/classes/ui/admin/partials/' );
 define( 'INGOT_ROOT', basename( dirname( __FILE__ ) ) );
 
-
+/**
+ * Actions to boot up plugin
+ */
 add_action( 'plugins_loaded', 'ingot_maybe_load', 0 );
+add_action( 'ingot_loaded',  'ingot_edd_sl_init', 1 );
+
+
+/**
+ * Load plugin if possible
+ *
+ * @since 0.0.0
+ */
 function ingot_maybe_load() {
 	$fail = false;
 	if ( ! version_compare( PHP_VERSION, '5.5.0', '>=' ) ) {
@@ -59,12 +69,16 @@ function ingot_maybe_load() {
 
 /**
  * EDD Licensing
+ *
+ * @since 0.3.0
  */
-define( 'INGOT_SL_STORE_URL', 'http://ingothq.com' );
-define( 'INGOT_SL_ITEM_NAME', 'Ingot Plugin: The Automatic A/B Tester' );
-add_action( 'admin_init', 'ingot_sl_plugin_updater', 0 );
-add_action( 'admin_init', 'ingot_sl_register_option');
-if( is_admin() && ! class_exists( 'EDD_SL_Plugin_Updater' ) ) {
-	include( dirname( __FILE__ ) . '/includes/EDD_SL_Plugin_Updater.php' );
+function ingot_edd_sl_init(){
+	define( 'INGOT_SL_STORE_URL', 'http://ingothq.com' );
+	define( 'INGOT_SL_ITEM_NAME', 'Ingot Plugin: The Automatic A/B Tester' );
+	add_action( 'admin_init', 'ingot_sl_plugin_updater', 0 );
+	add_action( 'admin_init', 'ingot_sl_register_option' );
+	if ( is_admin() && ! class_exists( 'EDD_SL_Plugin_Updater' ) ) {
+		include( dirname( __FILE__ ) . '/includes/EDD_SL_Plugin_Updater.php' );
+	}
 }
 
